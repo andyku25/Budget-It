f7512098d10d9a8b885ab256fea2507b1a69ad18
Andrew <andrew.ku93@gmail.com>
2021-06-15 13:05:49 -0400

    add loginform state and input/submit logic. add axios post request logic w/ response data

--- client/src/components/login/LoginForm.jsx
+++ client/src/components/login/LoginForm.jsx
@@ -1,14 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import { TextField, Button } from "@material-ui/core"
 
 
 const LoginForm = () => {
+
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: ""
+  })
+
+  const resetForm = () => {
+    setLoginData({
+      email: "",
+      password: ""
+    })
+  }
+
+  const handleInput = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+
+    setLoginData({
+      ...loginData,
+      [name]: value
+    })
+  }
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+
+    axios.post("/sessions", loginData)
+      .then((res) => {
+        console.log(res.data)
+        resetForm()
+      })
+  }
+
   return (
     <>
-      <form action="/sessions" method="POST" >
-        <TextField label="Email" type="email" variant="outlined" />
-        <TextField label="Password" type="password" variant="outlined" />
+      <form onSubmit={handleLogin} >
+        <TextField label="Email" type="email" variant="outlined" name="email" value={loginData.email} onChange={handleInput} />
+        <TextField label="Password" type="password" variant="outlined" name="password" value={loginData.password} onChange={handleInput} />
         <Button type="submit" variant="contained" color="primary" >Login</Button>
 
       </form>
